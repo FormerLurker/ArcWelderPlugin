75bfbee0a627af2e71ccd81ec5aa2ae5bb426a29
Formerlurker <formerlurker@pm.me>
2020-10-25 15:09:26 -0500

    Add license text.

--- octoprint_arc_welder/static/js/pnotify_extensions.js
+++ octoprint_arc_welder/static/js/pnotify_extensions.js
@@ -1,3 +1,29 @@
+/*################################################################################
+# Arc Welder: Anti-Stutter
+#
+# A plugin for OctoPrint that converts G0/G1 commands into G2/G3 commands where possible and ensures that the tool
+# paths don't deviate by more than a predefined resolution.  This compresses the gcode file sice, and reduces reduces
+# the number of gcodes per second sent to a 3D printer that supports arc commands (G2 G3)
+#
+# Copyright (C) 2020  Brad Hochgesang
+# #################################################################################
+# This program is free software:
+# you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see the following:
+# https://github.com/FormerLurker/ArcWelderPlugin/blob/master/LICENSE
+#
+# You can contact the author either through the git-hub repository, or at the
+# following email address: FormerLurker@pm.me
+################################################################################### */
+
 $(function () {
     var createPNotifyExtensions = function (options) {
         var self = this;
