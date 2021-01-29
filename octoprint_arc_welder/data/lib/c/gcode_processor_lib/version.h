27c7a8053fe534edb4a37a843109b0dc66512913
Formerlurker <formerlurker@pm.me>
2021-01-29 15:50:26 -0600

    Update ArcWelderLib version header.

--- octoprint_arc_welder/data/lib/c/gcode_processor_lib/version.h
+++ octoprint_arc_welder/data/lib/c/gcode_processor_lib/version.h
@@ -2,11 +2,11 @@
 // In version.h.in
 #ifndef VERSION_GENERATED_H
 	#define VERSION_GENERATED_H
-	#define GIT_BRANCH "devel"
-	#define GIT_COMMIT_HASH "3af9af2"
-	#define GIT_TAGGED_VERSION "1.0.1-11-g3af9af2"
-	#define GIT_TAG "1.0.1"
-	#define BUILD_DATE "2021-01-18T18:35:20Z"
+	#define GIT_BRANCH "master"
+	#define GIT_COMMIT_HASH "11d11e4"
+	#define GIT_TAGGED_VERSION "1.1.0"
+	#define GIT_TAG "1.1.0"
+	#define BUILD_DATE "2021-01-29T21:49:23Z"
 	#define COPYRIGHT_DATE "2021"
 	#define AUTHOR "Brad Hochgesang"
 #endif
